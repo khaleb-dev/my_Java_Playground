da9c85758d1ac59b4fce793ed54313b30d40bdf8
Khaleb O'Brien <ebukauche52@gmail.com>
2022-02-15 10:40:35 +0100

    employee

---
+++ employee/Employee.java
@@ -0,0 +1,40 @@
+package employee;
+
+public class Employee {
+    private String surName;
+    private String firstName;
+    private double salary;
+    private double tax = 0.05;
+
+    public Employee(String surName, String firstName, double salary) {
+        this.surName = surName;
+        this.firstName = firstName;
+        this.salary = salary;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    
+}
---
+++ employee/EmployeeRun.java
@@ -0,0 +1,13 @@
+package employee;
+
+public class EmployeeRun {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Smith", "John", 1000);
+        System.out.println(employee.getSurName());
+        System.out.println(employee.getFirstName());
+        System.out.println(employee.getSalary());
+        System.out.println(employee.getTax());
+    }
+}
+    
+}
