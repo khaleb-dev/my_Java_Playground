30cecc205acfa13d6aa9fbb458644a209c3c288d
Khaleb O'Brien <ebukauche52@gmail.com>
2022-02-21 01:26:55 +0100

    doubleDimArray attempt

---
+++ doubleDim/DoubleDimArray.java
@@ -0,0 +1,44 @@
+package doubleDim;
+
+public class DoubleDimArray {
+    private int[][] array;
+    private int rows;
+    private int columns;
+
+    public DoubleDimArray(double[][] data, int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        array = new int[rows][columns];
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public int get(int row, int column) {
+        return array[row][column];
+    }
+
+    public void set(int row, int column, int value) {
+        array[row][column] = value;
+    }
+    
+    
+    private double[][] data;
+    private int rows;
+    private int cols;
+
+    public DoubleDimArray(double[][] data, int rows, int cols) {
+        this.data = data;
+        this.cols = cols;
+        this.rows = rows;
+    }
+
+    public double myRowAverage(int row) {
+        return row;
+    }
+}
---
+++ doubleDim/DoubleDimArrayRun.java
@@ -0,0 +1,8 @@
+package doubleDim;
+
+public class DoubleDimArrayRun {
+    public static void main(String[] args) {
+        double[][] data = new double[2][4];
+        DoubleDimArray doubleDimArray = new DoubleDimArray(data, 4, 5);
+    }
+}
